9f1dffb0300ede59f8afb4adc7aceaebc9d9bc0e
Jake Anderson <barykade@umich.edu>
2015-12-04 17:21:30 -0500

    move mode tab

--- web/js/App.js
+++ web/js/App.js
@@ -114,18 +114,6 @@ $(document).ready(function() {
 
     loadMapData(1);
 
-    jQuery('.tabs .tab-links a').on('click', function(e)  {
-        var currentAttrValue = jQuery(this).attr('href');
- 
-        // Show/Hide Tabs
-        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
- 
-        // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
- 
-        e.preventDefault();
-    });
-
     preload(
         "tiles/scallywags_t1.png",
         "tiles/scallywags_t2.png",
--- web/js/View.js
+++ web/js/View.js
@@ -46,6 +46,18 @@ MapEditor.View = (function() {
         $("input#author").val($.cookie("author") || "");
     }
 
+    jQuery('.tabs .tab-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+ 
+        // Show/Hide Tabs
+        jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
+
     $("button#mapSelectionBtn").click(function(e) {
         e.preventDefault();
         e.stopImmediatePropagation();
