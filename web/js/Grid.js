622c6937997f6706ea74bacfad8c4c8fcafc03e4
dmartin <umdjm@hotmail.com>
2015-12-05 11:27:48 -0500

    health fix

--- web/js/Grid.js
+++ web/js/Grid.js
@@ -204,7 +204,7 @@ MapEditor.Grid.prototype.setClasses = function(array, ctx) {
         this.Hexes[h].class = current.class;
         this.Hexes[h].unitClass = current.unitClass;
         //this.Hexes[h].playerNum = current.playerNum;
-        //this.Hexes[h].health = current.health;
+        this.Hexes[h].health = current.health;
         this.Hexes[h].updateImage();
         this.Hexes[h].draw(ctx);
     }
